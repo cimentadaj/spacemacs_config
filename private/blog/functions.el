541b4364fdf7751d51133f538d0c8696a6539960
Jorge Cimentada <cimentadaj@gmail.com>
2020-11-26 10:15:15 +0100

    Moves blog functions to separate file

--- private/blog/functions.el
+++ private/blog/functions.el
@@ -1,66 +1,11 @@
-;;; packages.el --- blog layer packages file for Spacemacs.
-;;
-;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
-;;
-;; Author: Jorge <jorge@cimentadaj-Apollo>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
+;;; Package --- Summary
 
 ;;; Commentary:
 
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `blog-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `blog/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `blog/pre-init-PACKAGE' and/or
-;;   `blog/post-init-PACKAGE' to customize the package as it is loaded.
-
 ;;; Code:
 
-(defconst blog-packages
-  '()
-  "The list of Lisp packages required by the blog layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
 (defun find-blog ()
-  "Returns the regexp found in the known list of
-   projectile projects."
+  "Return the regexp found in the known list of projectile projects."
   ;; regexp are a bit different in emacs so I struggled
   ;; a bit to find a way to build this function. The idea
   ;; is to make this function search the (projectile-relevant-known-projects)
@@ -69,7 +14,7 @@ Each entry is either:
   "~/repositories/cimentadaj.github.io")
 
 (defun new-blog-post-R ()
-  "Skeleton of R function to create new blogpost using blogdown"
+  "Skeleton of R function to create new blogpost using blogdown."
   "
   my_new_post <- function(title, ...) {
 
@@ -86,7 +31,7 @@ Each entry is either:
   }")
 
 (defun blog/new-blog-post-blogdown ()
-"Run cimentadaj::my_new_post() on an argument read from the mini buffer.
+  "Run cimentadaj::my_new_post() on an argument read from the mini buffer.
 This effectively creates the Rmd file on the result of (find-blog) creating
 all the necessary folders in between. Finally, it opens the file as a new buffer."
   (interactive)
@@ -100,11 +45,7 @@ all the necessary folders in between. Finally, it opens the file as a new buffer
     (with-temp-buffer
       (insert-file-contents "/tmp/log.txt")
       (forward-line 2)
-      (delete-char 1)
-      (delete-char 1)
-      (delete-char 1)
-      (delete-char 1)
-      (delete-char 1)
+      (delete-char 5)
       (mwim-end-of-line)
       (backward-delete-char-untabify 1)
       (mwim-beginning-of-line)
@@ -112,5 +53,3 @@ all the necessary folders in between. Finally, it opens the file as a new buffer
       (shell-command "rm -rf /tmp/log.txt")
       (find-file (concat (find-blog) "/" (car kill-ring))))
     ))
-
-;;; packages.el ends here
